c7c79c61edfed0100ca5a122d6edfc793a7dbd02
chikeem90 <chikeem90@gmail.com>
2021-04-04 21:26:17 +0900

    feat: useScroll 커밋

---
+++ src/useScroll.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const useScroll = () => {
+    const [state, setState] = useState({
+      x: 0,
+      y: 0
+    });
+    const onScroll = () => {
+      setState({x: window.scrollX, y: window.scrollY});
+      console.log("y ", window.scrollY, "x ", window.scrollX);
+    }
+    useEffect(() => {
+      window.addEventListener("scroll", onScroll);
+      return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+    return state;
+  }
+  
+export default useScroll;
