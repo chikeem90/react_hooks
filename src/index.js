7c43c2276355d4ecb95b3ea942170dbbea3bdb11
chikeem90 <chikeem90@gmail.com>
2021-04-04 23:12:28 +0900

    feat: useAxios 커밋

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
-import useClick from "./useClick";
+import useAxios from "./useAxios";
 
 const App = () => {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
+  const { loading, data, refetch } = useAxios({ 
+    url: "https://yts.mx/api/v2/list_movies.json" 
+  });
   return (
-    <div className="App">
-      <h1 ref={title}>Hi</h1>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1>{ data && data.status }</h1>
+      <h2>{ loading && "Loading" }</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
