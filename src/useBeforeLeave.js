164275100c446ec75021ad558b39c8995d193fea
chikeem90 <chikeem90@gmail.com>
2021-04-04 20:24:37 +0900

    feat: useBeforeLeave 커밋

---
+++ src/useBeforeLeave.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const useBeforeLeave = (onBefore) => {
+    const handle = (event) => {
+      const { clientY } = event;
+      if (clientY <= 0) {
+        onBefore();
+      }
+    }
+    useEffect(() => {
+      document.addEventListener("mouseleave", handle);
+      return () => document.removeEventListener("mouseleave", handle);
+    }, []);
+  }
+  
+export default useBeforeLeave;
